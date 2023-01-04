4e3970ec00b6496400619136473d8003fc0d3660
Ahmed K Madani <ahmedkamal543@gmail.com>
2023-01-04 20:12:27 +0300

    add dockerFile and docker-compose

--- democrance/settings.py
+++ democrance/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "app",
+     "app",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DATETIME_FORMAT": "%d-%m-%Y",
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
